26084170b0a5c24e1976cdc5a19b9f705a104063
Dan <dan.thomason@lbi-technology.com>
2020-07-24 21:43:15 +0100

    Clean up.

--- testpage.php
+++ testpage.php
@@ -7,7 +7,7 @@
     <img src="clear-nta-logo.png" alt="Northern Training Academy" width="130" height="86" style="padding-left: 20px; padding-top: 20px; padding-bottom: 60px;">
     <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="GET" class="pure-form pure-form-stacked" style="padding-left: 20px; padding-bottom: 60px;" >
         <fieldset>
-            <legend> Learning Mentor Level 3 - Activity 1 <?php echo "Hello NTA"; ?></legend><br><br><br>
+            <legend> Learning Mentor Level 3 - Activity 1 </legend><br><br><br>
             <div class="pure-g" style="padding-bottom: 60px;">
                 <div class="pure-u-1 pure-u-md-1-3">
                     <label for="multi-first-name">This is an example question</label><br>
