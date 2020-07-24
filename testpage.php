365a24e7d1a89129d7f576ec4d9421794503a292
Dan <dan.thomason@lbi-technology.com>
2020-07-24 21:37:29 +0100

    GET AMENDS

--- testpage.php
+++ testpage.php
@@ -5,16 +5,16 @@
 <body>
 
     <img src="clear-nta-logo.png" alt="Northern Training Academy" width="130" height="86" style="padding-left: 20px; padding-top: 20px; padding-bottom: 60px;">
-<?php print_r($_POST); ?>
-    <?php echo 'Value returned = ' . $_POST["multi-first-name"]; ?><br>
+<?php print_r($_GET); ?>
+    <?php echo 'Value returned = ' . $_GET["comment_data"]; ?><br>
 <?php echo 'Web page = ' . $_SERVER["PHP_SELF"]; ?><br>
-    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST" class="pure-form pure-form-stacked" style="padding-left: 20px; padding-bottom: 60px;" >
+    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="GET" class="pure-form pure-form-stacked" style="padding-left: 20px; padding-bottom: 60px;" >
         <fieldset>
             <legend> Learning Mentor Level 3 - Activity 1 <?php echo "Hello NTA"; ?></legend><br><br><br>
             <div class="pure-g" style="padding-bottom: 60px;">
                 <div class="pure-u-1 pure-u-md-1-3">
                     <label for="multi-first-name">This is an example question</label><br>
-                    <textarea rows="5" cols="80" name="doodaa" id="multi-first-name" class="pure-u-23-24"/></textarea><br><br>
+                    <textarea rows="5" cols="80" name="comment_data" id="multi-first-name" class="pure-u-23-24"/></textarea><br><br>
                 </div>
                         <br><br>
             <button type="submit" class="pure-button pure-button-primary">Save</button>
