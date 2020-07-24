62389a158427182b223502e8d8c2a49b6eea53ec
Dan <dan.thomason@lbi-technology.com>
2020-07-24 21:17:17 +0100

    testpage

--- testpage.php
+++ testpage.php
@@ -7,7 +7,7 @@
     <img src="clear-nta-logo.png" alt="Northern Training Academy" width="130" height="86" style="padding-left: 20px; padding-top: 20px; padding-bottom: 60px;">
 <?php echo 'Value returned = ' . $_GET["multi-first-name"]; ?><br>
 <?php echo 'Web page = ' . $_SERVER["PHP_SELF"]; ?><br>
-    <form method="get" action="<?php echo $_SERVER["PHP_SELF"];?>" class="pure-form pure-form-stacked" style="padding-left: 20px; padding-bottom: 60px;" >
+    <form method="get" action="testpage.php" class="pure-form pure-form-stacked" style="padding-left: 20px; padding-bottom: 60px;" >
         <fieldset>
             <legend> Learning Mentor Level 3 - Activity 1 <?php echo "Hello NTA"; ?></legend><br><br><br>
             <div class="pure-g" style="padding-bottom: 60px;">
