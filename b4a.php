cc32ec6d3fbc595b1ce988b08b6f32372b2609a1
Dan <dan.thomason@lbi-technology.com>
2020-07-25 13:45:51 +0100

    TEST SET

--- b4a.php
+++ b4a.php
@@ -9,12 +9,13 @@ use Parse\ParseClient;
 ParseClient::initialize( "6gUY2ACd17tsiDroxH6tmRTOx9P4S99LODf9P4lB", "qtLatc5USQoeYas8CUADRoUoJLWws0CP6a5inzmp", "nuKqVaJXBSsYJEg5p79KCY09pu2sU1pYzSvEEH8l" );
 ParseClient::setServerURL('https://parseapi.back4app.com', '/');
 
-use Parse\ParseQuery;
-
-$query = new ParseQuery("Activity");
-$results = $query->find();
-foreach($results as $result) {
-  echo 'Object found ' . $result->getObjectId() . '<br>';
+if (isset($_GET['action'])){
+    use Parse\ParseQuery;
+    $query = new ParseQuery("Activity");
+    $results = $query->find();
+    foreach($results as $result) {
+    echo 'Object found ' . $result->getObjectId() . '<br>';
+    }
 }
 
 ?>
