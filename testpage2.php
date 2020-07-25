b99367d09ff7afbdc748234ba7c18d1e3f7005e7
Dan <dan.thomason@lbi-technology.com>
2020-07-25 13:24:28 +0100

    Iframe

---
+++ testpage2.php
@@ -0,0 +1,24 @@
+<head>
+<link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
+
+</head>
+<body>
+
+    <img src="clear-nta-logo.png" alt="Northern Training Academy" width="130" height="86" style="padding-left: 20px; padding-top: 20px; padding-bottom: 60px;">
+    <iframe name="content" style="">
+    </iframe>
+    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="GET" class="pure-form pure-form-stacked" style="padding-left: 20px; padding-bottom: 60px;" target="content">
+        <fieldset>
+            <legend> Learning Mentor Level 3 - Activity 1 </legend><br><br><br>
+            <div class="pure-g" style="padding-bottom: 60px;">
+                <div class="pure-u-1 pure-u-md-1-3">
+                    <label for="multi-first-name">This is an example question</label><br>
+                    <textarea rows="5" cols="80" name="comment_data" id="multi-first-name" class="pure-u-23-24"/><?php echo $_GET["comment_data"]; ?></textarea><br><br>
+                </div>
+                        <br><br>
+            <button type="submit" class="pure-button pure-button-primary">Save</button>
+        </fieldset>
+    </form>
+</body>
+<footer>
+</footer>
