df49cb57b692c667f0a4b54c633ac128285bd94c
Dan <dan.thomason@lbi-technology.com>
2020-07-25 13:35:23 +0100

    b4a

---
+++ b4a.php
@@ -0,0 +1,19 @@
+<?php
+
+require '../parse-php-sdk/autoload.php';
+
+use Parse\ParseClient;
+
+// Initializes with the <APPLICATION_ID>, <REST_KEY>, and <MASTER_KEY>
+ParseClient::initialize( "6gUY2ACd17tsiDroxH6tmRTOx9P4S99LODf9P4lB", "qtLatc5USQoeYas8CUADRoUoJLWws0CP6a5inzmp", "nuKqVaJXBSsYJEg5p79KCY09pu2sU1pYzSvEEH8l" );
+ParseClient::setServerURL('https://parseapi.back4app.com', '/');
+
+use Parse\ParseQuery;
+
+$query = new ParseQuery("Activity");
+$results = $query->find();
+foreach($results as $result) {
+  echo 'Object found ' . $result->getObjectId() . '<br>';
+}
+
+?>
